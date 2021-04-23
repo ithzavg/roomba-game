323a0326afc6cb65547d400dfd30631bff96be48
ithzavg <ithgarvi@gmail.com>
2021-04-22 21:30:53 -0500

    Testing template literal and functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('hello its working');
+window.addEventListener('load', mira);
+
+const Robot = () => {
+    console.log("carga 2")
+    const  template = `<button id="button" onclick="mira()">Hola</button>`
+    return template;
+}
+
+const agrega = null || document.getElementById('testing');
+agrega.innerHTML = Robot();
+
+function mira(){
+    console.log('evento detectado 1');
+}
+
+
+
+/*const mira = () => {
+    console.log('evento detectado 1');
+}*/
+
+
+//const boton = document.getElementById('button');
+//boton.addEventListener("click", mira);
+
+
+
+
+
+
+
+
+
+
+
